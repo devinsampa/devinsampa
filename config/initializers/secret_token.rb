dcf9ffc676a6f254d9e9d3938ee647448b7154b2
tinogomes <tinorj@gmail.com>
2011-06-30 09:43:43 -0300

    Upgrade to Rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Devinsampa::Application.config.secret_token = '2f0220f55d182b03895d8e38c6d5019f9ec2673bb5cad7b47f7bcea125a8db23b028553fec388c8d5a1f95dd9b7866b7c3f064b8119a0e33bd5b22bff1d56b32'
